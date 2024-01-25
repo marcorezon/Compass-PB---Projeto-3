e7715c05c39d7de461fd2045f761d61bc283d1fa
Marco Rezon <marcorezonmoura1@gmail.com>
2024-01-24 22:17:43 -0400

    Added location data to select form inputs

--- src/shared/components/FormattedSelect.tsx
+++ src/shared/components/FormattedSelect.tsx
@@ -1,26 +1,36 @@
-import { Container, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
-import { locationData } from "../services/location/locationApi";
-import { useEffect } from "react";
-
+import { Box, FormControl, InputLabel, MenuItem, NativeSelect, Select, SelectChangeEvent } from "@mui/material";
 import countriesData from "./../../../utils/countries-and-cities";
+import { useState } from "react";
 
 export const FormattedSelect = () => {
 
     const countries = Object.keys(countriesData);
 
+    const [country, setCountry] = useState('');
+    const [city, setCity] = useState('');
+
+    function handleSelectCountry (event: SelectChangeEvent) {
+        setCountry(event.target.value as string);
+    }
+    function handleSelectCity (event: SelectChangeEvent) {
+        setCity(event.target.value as string);
+    }
+
     return (
-        <Container sx={{gap: 'inherit'}}>
+        <Box sx={{ display: 'flex', flexDirection: 'column', m: '0', p:'0', gap: '20px'}}>
+
             <FormControl fullWidth>
                 <InputLabel id="demo-simple-select-label">Country</InputLabel>
                 <Select
                     
                     labelId="Country"
                     id="selectCountry"
-                    value={null}
+                    value={country}
                     label="Contry"
-                    onChange={undefined}
+                    onChange={handleSelectCountry}
                 >
                     {countries.map( (country) => {
+                        console.log(country)
                         return (
                             <MenuItem key={country} value={country}>{country}</MenuItem>
                         );
@@ -29,21 +39,22 @@ export const FormattedSelect = () => {
                 </Select>
             </FormControl>
 
-            <FormControl fullWidth>
+            <FormControl disabled={country === ''} fullWidth>
                 <InputLabel id="demo-simple-select-label">City</InputLabel>
                 <Select
-                    
-                    labelId="Country"
-                    id="selectCountry"
-                    value={null}
-                    label="Contry"
-                    onChange={undefined}
-                >      
-                    <MenuItem value={10}>Ten</MenuItem>
-                    <MenuItem value={20}>Twenty</MenuItem>
-                    <MenuItem value={30}>Thirty</MenuItem>
+                    labelId="City"
+                    id="selectCity"
+                    value={city}
+                    label="City"
+                    onChange={handleSelectCity}
+                >   
+                    {!(country === "") && (countriesData[country].map( (city: string) => {
+                        return (
+                            <MenuItem key={city} value={city}>{city}</MenuItem>
+                        );}))}
+  
                 </Select>
             </FormControl>
-        </Container>
+        </Box>
     );
 }
